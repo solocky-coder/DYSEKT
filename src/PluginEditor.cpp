871086ceb3d190b6080fe1df549fcb9107c2038e
PAINT711 <solocky@gmail.com>
2026-03-04 14:39:13 +0100

    Add files via upload

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -105,6 +105,7 @@ DysektEditor::DysektEditor (DysektProcessor& p)
     headerBar.onBrowserToggle   = [this] { toggleBrowserPanel(); };
     headerBar.onWaveToggle      = [this] { toggleSoftWave(); };
     headerBar.onChromaticToggle = [this] { toggleChromatic(); };
+    headerBar.onBodeToggle      = [this] { toggleMixerPanel(); };  // ← CHANGE 1: bode icon opens mixer
 
     // Keep actionPanel callbacks as no-ops (buttons removed from action bar)
     actionPanel.onBrowserToggle    = nullptr;
@@ -144,7 +145,6 @@ int DysektEditor::computeTotalHeight() const
 
     return h;
 }
-
 
 void DysektEditor::toggleBrowserPanel()
 {
@@ -260,6 +260,7 @@ void DysektEditor::toggleMixerPanel()
 {
     mixerOpen = ! mixerOpen;
     mixerPanel.setVisible (mixerOpen);
+    headerBar.setBodeActive (mixerOpen);   // ← CHANGE 2: keeps bode icon highlighted while mixer is open
     setSize (getWidth(), computeTotalHeight());
     resized();
 }
