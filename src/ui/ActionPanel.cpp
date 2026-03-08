0987b5cf96ee18592b8540cb32d5ed3a3e2a6024
PAINT711 <solocky@gmail.com>
2026-03-08 15:40:49 +0100

    Refactored ActionPanel constructor to include tooltip initialization and removed unnecessary lines.

--- src/ui/ActionPanel.cpp
+++ src/ui/ActionPanel.cpp
@@ -1,8 +1,17 @@
 /*
 ...
 */
-addSliceBtn.setTooltip ("Add Slice (A / hold Alt)");
-lazyChopBtn.setTooltip ("MIDI SLICE");
+// The tooltip initialization should be part of the constructor:
+ActionPanel::ActionPanel(DysektProcessor& p, WaveformView& wv)
+    : processor(p), waveformView(wv)
+{
+    addSliceBtn.setTooltip("Add Slice (A / hold Alt)");
+    lazyChopBtn.setTooltip("MIDI SLICE");
+    // ... Other initialization ...
+}
 /*
 ...
 */
+
+// Remove the lines outside the constructor (original lines 4 and 5)
+// Only keep the above inside constructor.
