b0ffd20433bf3cd8fc07dca198da7370b2534b84
PAINT711 <solocky@gmail.com>
2026-03-12 05:22:51 +0100

    WaveformView.h UPDATE

--- src/ui/WaveformView.h
+++ src/ui/WaveformView.h
@@ -87,8 +87,6 @@ private:
     void paintLazyChopOverlay (juce::Graphics& g);
     void paintTransientMarkers (juce::Graphics& g);
     void paintTrimOverlay (juce::Graphics& g);
-    void paintTrimOverlay (juce::Graphics& g);
-    void paintTrimMarkers (juce::Graphics& g);
 
     // Aggregates all cache-invalidation inputs; rebuild is skipped when unchanged.
     struct CacheKey
