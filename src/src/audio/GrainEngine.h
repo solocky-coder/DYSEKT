0dcc7efa4425d81567fdc0fba20cd8b7415367d4
PAINT711 <solocky@gmail.com>
2026-04-11 13:55:25 +0200

    DELETE UNEEDED SRC FOLDER

--- src/src/audio/GrainEngine.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class GrainEngine
-{
-public:
-    static float calcStretchBpm (int startSample, int endSample, float timeUnitBars, double sampleRate);
-};
--- src/src/params/ParamLayout.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <juce_audio_processors/juce_audio_processors.h>
-
-namespace ParamLayout
-{
-    juce::AudioProcessorValueTreeState::ParameterLayout createLayout();
-}
